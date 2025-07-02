90964f537af2f735111bce52e68e29e48e1711c8
Jackson Moraes <jacksontadeu@hotmail.com>
2025-07-02 11:07:01 -0300

    :sparkles: feat: Criacao do respository, controller e do service

---
+++ src/main/java/com/deliverytech/delivery_api/repository/ProdutoRespository.java
@@ -0,0 +1,7 @@
+package com.deliverytech.delivery_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRespository extends JpaRepository{
+
+}
---
+++ src/main/java/com/deliverytech/delivery_api/repository/RestauranteRepository.java
@@ -0,0 +1,8 @@
+package com.deliverytech.delivery_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface RestauranteRepository  extends JpaRepository{
+
+}
