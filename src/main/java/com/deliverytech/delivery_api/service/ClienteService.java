62ffbbc8a223609f66bbca563f3b20b1f06e8fe2
Jackson Moraes <jacksontadeu@hotmail.com>
2025-07-04 07:44:51 -0300

    :sparkles: feat: Criação do restaurante repository

--- src/main/java/com/deliverytech/delivery_api/service/ClienteService.java
+++ src/main/java/com/deliverytech/delivery_api/service/ClienteService.java
@@ -1,18 +1,8 @@
 package com.deliverytech.delivery_api.service;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.deliverytech.delivery_api.entity.model.Cliente;
-import com.deliverytech.delivery_api.repository.ClienteRepository;
 
 public class ClienteService {
-    @Autowired
-    private ClienteRepository clienteRepository;
-
-    public List<Cliente> listarTodos(){
-        return clienteRepository.findAll();
-    }
+    
+   
 
 }
