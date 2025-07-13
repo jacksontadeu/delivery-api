5678215541dece3967f37fdb0d7fe1372e758de5
Jackson Moraes <jacksontadeu@hotmail.com>
2025-07-13 12:57:37 -0300

    ?sparkles: feat: Criacao do usuario repository

---
+++ src/main/java/com/jtmjinfo/delivery_api/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.jtmjinfo.delivery_api.repository;
+
+import com.jtmjinfo.delivery_api.entity.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<UsuarioRepository, Long> {
+    Optional<Usuario> findByEmail(String email);
+}
